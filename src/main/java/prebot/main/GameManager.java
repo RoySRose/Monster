8bcc36b47df5df549eba65ffb3d377ec67241655
Roy Kim <sungwook0115.kim@gmail.com>
2018-11-25 14:53:44 +0900

    [#initial] Boostrap main

---
+++ src/main/java/prebot/main/GameManager.java
@@ -0,0 +1,43 @@
+package prebot.main;
+
+import prebot.common.debug.StopWatch;
+
+/**
+ * @author insaneojw
+ */
+public abstract class GameManager {
+
+    private int timeToLiveFrames = 0;
+    private long recorded = 0L;
+    private StopWatch stopWatch = new StopWatch();
+
+    public long getRecorded() {
+        return recorded;
+    }
+
+    public void setStopWatchTag(String tag) {
+        stopWatch.setTag(tag);
+    }
+
+    public void start() {
+        stopWatch.start();
+    }
+
+    public void updateTimeCheck() {
+        start();
+        update();
+        end();
+    }
+
+    public abstract void update();
+
+    public void end() {
+        long timeSpent = stopWatch.record();
+        if (timeSpent > recorded || timeToLiveFrames <= 0) {
+            recorded = timeSpent;
+            timeToLiveFrames = 180;
+        } else {
+            timeToLiveFrames--;
+        }
+    }
+}
