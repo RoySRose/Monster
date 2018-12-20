3da91804cdfca19e4ed584ee36ae59db6b46f9c1
jw <jerrioh@gmail.com>
2018-12-21 00:08:04 +0900

    second commit

--- src/main/java/org/monster/micro/SquadData.java
+++ src/main/java/org/monster/micro/SquadData.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Deprecated
 public class SquadData {
 
     private Map<String, Squad> squadMap = new HashMap<>(); // 이름별 squad 저장
